a51ce58d298de903a437f2f8b59ef9d1e9244f03
sergey s <som-ana@yandex.ru>
2023-04-07 22:21:22 +0300

    initial commit

---
+++ src/calculationOfBonusMiles.java
@@ -0,0 +1,15 @@
+public class calculationOfBonusMiles {
+    public static void main(String[] args) {
+
+        int amount = 12678; //стоимость билета
+
+        int mileToRub = 20; //1 миля равна 20 руб
+
+        int bonusMiles = amount / mileToRub;
+
+        System.out.println("бонусных миль " + bonusMiles);
+
+    }
+
+}
+
